5d490793d925e0ced1eb2c619ab0638444ab5c93
Carl Jeske <carljeske@googlemail.com>
2017-08-22 19:26:36 +0100

    Add a new player who always aims for the middle

--- noughts/player.py
+++ noughts/player.py
@@ -2,5 +2,8 @@ import random
 
 
 class Player:
-    def __init__(self):
-        self.get_moves = lambda x: random.sample(range(9), k=9)
+    def __init__(self, smart=False):
+        if smart:
+            self.get_moves = lambda x: [4] + random.sample([0, 1, 2, 3, 5, 6, 7, 8], k=8)
+        else:
+            self.get_moves = lambda x: random.sample(range(9), k=9)
